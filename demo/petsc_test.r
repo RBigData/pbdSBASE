2df1080741fc1d206dbb28a78da7df569b1ac13a
wrathematics <wrathematics@gmail.com>
2014-01-26 21:44:27 -0500

    01.26.14 demo

---
+++ demo/petsc_test.r
@@ -0,0 +1,40 @@
+library(pbdSBASE, quietly=TRUE)
+library(pbdDMAT, quietly=TRUE)
+init()
+
+
+petsc_init()
+
+petsc_matprinter("default")
+petsc_test()
+
+petsc_finalize()
+
+
+m <- 9
+n <- 8
+x <- matrix(c(
+              rep(0, n),
+              1, 0, 2, 0, 4, 0, 6, 0, 
+              rep(0, n), 
+              20, 0, 22, 0, 24, 0, 26, 0, 
+              rep(0, n), 
+              40, 0, 42, 0, 44, 0, 46, 0, 
+              rep(0, n), 
+              60, 0, 62, 0, 64, 0, 66, 0, 
+              rep(0, n)), 
+            m, n, byrow=T)
+
+
+
+#pbdDMAT:::convert_dense_to_csr(x)
+
+
+l <- pbdDMAT:::convert_dense_to_csr(x)
+y <- pbdDMAT:::convert_csr_to_dense(dim(x), l$Data, l$row_ptr, l$col_ind)
+
+all.equal(x, y)
+
+
+
+finalize()
